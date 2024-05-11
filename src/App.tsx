60dbeec8a1b44a220405be522d58a82d87e0f2af
Kasia <kasia1781@gmail.com>
2024-05-11 18:32:14 +0200

    added Product component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import Header from './components/Header';
 import Product from './components/Product';
 import Shop from './components/Shop';
 import logoImg from '/logo.png';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+
+console.log(DUMMY_PRODUCTS);
 
 function App() {
+	type Product = {
+		id: string;
+		image: string;
+		title: string;
+		price: number;
+		description: string;
+	};
+
 	return (
 		<>
 			<Header image={{ src: logoImg, alt: 'A women' }} />
 			<Shop>
-				<Product />
+				{DUMMY_PRODUCTS.map((product: Product) => (
+					<li key={product.id}>
+						<Product product={product} />
+					</li>
+				))}
 			</Shop>
 		</>
 	);
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,8 +1,27 @@
-export default function Product() {
-    return (
-        <article className="product">
-            
-            
-        </article>
-    )
+type ProductProps = {
+	product: {
+		id: string;
+		image: string;
+		title: string;
+		price: number;
+		description: string;
+	};
+};
+
+export default function Product({ product }: ProductProps) {
+	return (
+		<article className='product'>
+			<img src={product.image} alt={product.title} />
+			<div className='product-content'>
+				<div>
+					<h3>{product.title}</h3>
+					<p className='product-price'>${product.price}</p>
+					<p>{product.description}</p>
+				</div>
+				<div className='product-actions'>
+					<button>Add To Card</button>
+				</div>
+			</div>
+		</article>
+	);
 }
